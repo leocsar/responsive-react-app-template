3536b6649ab2069c5ee41ee8858b68d312a5516e
leo_cesar <leoccet@gmail.com>
2020-12-30 08:36:03 -0300

    Correção no arquivo de estilos da página inicial

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   background: ${props => props.theme.colors.background};
-  color #FFF;
+  color: #FFF;
   display: flex;
   justify-content: center;
   align-items: center;
